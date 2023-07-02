93847fc3a5a341a537fefd0dbf173cd6366ec915
DimaSouthTree <DITsindrin@gmail.com>
2023-07-02 16:23:20 +0300

    defining classes for a task, update pyproject.toml and poetry.lock

--- parsing.py
+++ parsing.py
@@ -1,18 +1,34 @@
+from abc import ABC, abstractmethod
 from pprint import pprint
-
 import requests
 import json
 
-def get_service():
-    hh_info = requests.get("https://api.hh.ru/vacancies")
-    return hh_info.text
 
-# pprint(get_service())
+class Api_get_servise(ABC):
+    pass
 
 
+class Api_get_servise_hh(Api_get_servise):
+    pass
 
-def get_service_job():
-    job_info = requests.get("https://api.superjob.ru/2.0/vacancies",headers={"X-Api-App-Id": "v3.r.137645209.b47468e74b48a5fb3a8923a38104bf7bf9405bc5.c332503b8cc95ce4034c399dae34c4042e4a09bc"})
-    return job_info.text
 
-print(get_service_job())
+class Api_get_servise_sjob(Api_get_servise):
+    pass
+
+
+class Vacancy():
+    def __init__(self, name_vacancy, url_vacancy, salary_vacancy, requirements_vacancy):
+        self.name = name_vacancy
+        self.url = url_vacancy
+        self.salary = salary_vacancy
+        self.requirements = requirements_vacancy
+
+    pass
+
+
+class Save_to_file(ABC):
+    pass
+
+
+class JsonSaveFail(Save_to_file):
+    pass
